7136151b40934f88183786c830be03f0b62f752b
palakberi <palakberi25@gmail.com>
2025-11-08 03:07:35 +0530

    completed backend+frontend

--- src/main/java/com/inventory_management/inventory_management/controllers/ProductController.java
+++ src/main/java/com/inventory_management/inventory_management/controllers/ProductController.java
@@ -1,9 +1,77 @@
 package com.inventory_management.inventory_management.controllers;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
-@Service
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.inventory_management.inventory_management.model.Product;
+import com.inventory_management.inventory_management.services.ProductService;
+
+@Controller
 public class ProductController {
     
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/")
+    public String getIndexPage(Model model) {
+        List<Product> productList = productService.getAllProducts();
+        model.addAttribute("products", productList);
+        model.addAttribute("product", new Product());
+        return "index";
+    }
+
+    @PostMapping("/newProduct")
+    public String newProduct(@ModelAttribute("product") Product product, RedirectAttributes redirectAttributes) {
+        // Creating dynamic product ID
+        String productId = "PD" + (1000 + new Random().nextInt(9000));
+        product.setId(productId);
+        productService.addProduct(product);
+        redirectAttributes.addFlashAttribute("insertSuccess", true);
+        return "redirect:/";
+    }
+
+        @PostMapping("/searchProduct")
+    public String searchProduct(@RequestParam(name = "id") String id, RedirectAttributes redirectAttributes,
+            Model model) {
+        Optional<Product> foundProduct = productService.getProductById(id);
+        model.addAttribute("product", new Product());
+        if (foundProduct.isPresent()) {
+            redirectAttributes.addFlashAttribute("foundProduct", foundProduct);
+        } else {
+            redirectAttributes.addFlashAttribute("notFound", true);
+        }
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/deleteProduct")
+    public String deleteProduct(@RequestParam(name = "id") String id, RedirectAttributes redirectAttributes) {
+        boolean deleteSuccess = productService.deleteProductById(id);
+        if (deleteSuccess) {
+            redirectAttributes.addFlashAttribute("deleteSuccess", true);
+        } else {
+            redirectAttributes.addFlashAttribute("notFound", true);
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/productAnalysis")
+    public String productAnalysis(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "productAnalysis"; // Thymeleaf template name
+    }
+
 
 }
